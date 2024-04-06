2690b9fad217b1d3dc74d28069638b5078f0d9e7
Alejandro González Reyes <alexglzrys@gmail.com>
2024-04-06 10:30:03 -0600

    inicio del proyecto

---
+++ lib/screens/full_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FullScreen extends StatelessWidget {
+  const FullScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Pantalla Completa'),
+      ),
+    );
+  }
+}
